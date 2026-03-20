0751fba85f43d73a613a0444ec52ecf939694eb2
Beatriz Silva <biasrocha@uol.com.br>
2026-03-20 15:37:46 -0300

    Criação dos models Solicitacao e Problema

---
+++ ConsultoriaDev/Models/Problema.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsultoriaDev.Models
+{
+    public class Problema
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
+        public int TempoResposta { get; set; }
+    }
+}
---
+++ ConsultoriaDev/Models/Solicitacao.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsultoriaDev.Models
+{
+    public class Solicitacao
+    {
+        public string NomeCliente { get; set; }
+        public string EmailCliente { get; set; }
+        public string DescricaoProblema { get; set; }
+        public DateTime DataSolicitacao { get; set; }
+        public string Status { get; set; }
+    }
+}
