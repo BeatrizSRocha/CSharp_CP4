0dba0ee605c0aacb3582ac525e8e0b565c9d8bf7
Larissa Estella <larissaestella56@gmail.com>
2026-03-21 14:03:34 -0300

    adiciona view de login do admin

--- ConsultoriaDev/Models/Solicitacao.cs
+++ ConsultoriaDev/Models/Solicitacao.cs
@@ -5,7 +5,8 @@
         public string NomeCliente { get; set; }
         public string EmailCliente { get; set; }
         public string DescricaoProblema { get; set; }
-        public DateTime DataSolicitacao { get; set; }
-        public string Status { get; set; }
+        public int TempoResposta { get; set; }
+        public DateTime DataSolicitacao { get; set; } = DateTime.Now;
+        public string Status { get; set; } = "Pendente";
     }
 }
--- ConsultoriaDev/obj/Debug/net8.0/ConsultoriaDev.AssemblyInfo.cs
+++ ConsultoriaDev/obj/Debug/net8.0/ConsultoriaDev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultoriaDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0751fba85f43d73a613a0444ec52ecf939694eb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c975c4ae0607810eb21b8ce0287a6934dc3611b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultoriaDev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultoriaDev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
