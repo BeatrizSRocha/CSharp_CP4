c975c4ae0607810eb21b8ce0287a6934dc3611b0
Larissa Estella <larissaestella56@gmail.com>
2026-03-21 13:22:25 -0300

    adiciona AdminController

--- ConsultoriaDev/obj/Debug/net8.0/ConsultoriaDev.AssemblyInfo.cs
+++ ConsultoriaDev/obj/Debug/net8.0/ConsultoriaDev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultoriaDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0751fba85f43d73a613a0444ec52ecf939694eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultoriaDev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultoriaDev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
